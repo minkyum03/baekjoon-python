3d48b77028a6ff078d094f9166c45af0ba756533
minkyum03 <kimminkyum03@gmail.com>
2025-07-18 19:10:32 +0900

    백준 11718번 문제 업로드

---
+++ 2025-07-18/11718.py
@@ -0,0 +1,17 @@
+# # 백준 11718번
+# # 입력:
+# Hello
+# Baekjoon
+# Online Judge
+
+# # 출력:
+# Hello
+# Baekjoon
+# Online Judge
+
+try:
+    while True:
+        word = input()
+        print(word)
+except EOFError:
+    pass
